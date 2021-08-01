1f29d10525d0f66e4a0a1e85f1d52630bac4bceb
Laura Markovičová <markovicova.laura@gmail.com>
2021-08-01 18:15:32 +0200

    final version

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+
+import '../../App.css';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  )
+}
+
+export default Home;
